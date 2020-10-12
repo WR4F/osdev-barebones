fc017287d0bc015a431af2800ddf3c5da49d71bb
rslay <25256438+rslay@users.noreply.github.com>
2020-10-12 19:46:35 -0400

    Fix to build.ps1

--- build.ps1
+++ build.ps1
@@ -15,16 +15,16 @@ win-i686-elf\bin\i686-elf-as.exe boot.s -o build\boot.o
 win-i686-elf\bin\i686-elf-gpp.exe -T linker.ld -o build\myos.bin -ffreestanding -O2 -nostdlib build\boot.o build\kernel.o -lgcc
 
 
-# Package kernel into a bootable ISO with grub
+# Package kernel into a bootable binary
 mkdir isobuild\boot\grub
 cp build\myos.bin isobuild\boot\myos.bin
 cp grub.cfg isobuild\boot\grub\grub.cfg
+
+# Build ISO from binary, package with GRUB
 bash -c "grub-file --is-x86-multiboot build/myos.bin && echo 'Kernel bootable with GRUB!'"
 bash -c "grub-mkrescue -o build/myos.iso isobuild 2> /dev/null && echo 'ISO ready!'"
-
-
 # Launch with qemu
 qemu-system-i386 -cdrom build\myos.iso
 
-# For faster testing, use this instead
+# For faster testing, use this instead of building ISO
 # qemu-system-i386 -kernel build\myos.bin
